a18a241db343001988af5d9975c51bb7274af6b6
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-04-29 16:11:52 +0530

    first commit

---
+++ 02_DataTypes/PrimitiveDemo.java
@@ -0,0 +1,12 @@
+public class PrimitiveDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        double salary= 20000.75;
+        char initial = 'J';
+        boolean isEnrolled = true;
+
+        System.out.println("Age: "+ age);
+        System.out.println("Salary: "+ salary);
+        System.out.println("Initials: "+ initial);
+    }
+}
---
+++ 02_DataTypes/StringDemo.java
@@ -0,0 +1,12 @@
+public class StringDemo {
+    public static void main(String[] args) {
+    String name = "Groot Academy";
+    String greeting = null;
+    
+    System.out.println("School: "+ name);
+    System.out.println("Greeting: "+ greeting);
+
+    greeting = "Welcom to java!";
+    System.out.println("Update greeting: "+ greeting);
+    }
+}
