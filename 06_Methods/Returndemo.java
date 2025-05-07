f173b18dcdfb04848b2bf68197ef1f252df03b11
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-05-07 15:26:38 +0530

    added more programs

---
+++ 06_Methods/Returndemo.java
@@ -0,0 +1,9 @@
+public class Returndemo {
+    public static void main(String[] args) {
+        int result = multiply(4, 5);
+        System.out.println("Result" + result);
+    }
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+};
