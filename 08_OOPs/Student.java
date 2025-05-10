48470618de25839e76ad53edc00fa9606d1d684f
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-05-10 13:25:01 +0530

    added more programs

--- 08_OOPs/Student.java
+++ 08_OOPs/Student.java
@@ -1,5 +1,19 @@
 public class Student {
-    public static void main(String[] args) {
-        
+
+    String name;
+    int age;
+    float grade;
+
+    // methods
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age + ", Grade: " + grade); 
     }
-}
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Alice";
+        s1.age = 24;
+        s1.grade = 88.5f;
+        s1.display();
+    }
+};
