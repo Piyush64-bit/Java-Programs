4092b0458fa299c7de02abdd7102c10c46445d7e
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-05-21 23:42:34 +0530

    added more programs

---
+++ 09_ArrayAndString/Notes.java
@@ -0,0 +1,12 @@
+// in java there is no pointer concept
+// array is a collection of similar type of data
+
+// array is a collection of similar and multiple type of data
+// array objects are stored in  The heap memory 
+// Heap opject are not onctinous 
+// dynamic memory allocation to the place in the heap memory
+// the new keyword is used to create a new object in the heap memory
+//try to declare an array assigning the size , you will not get any error but you will get a runtime error
+// string is a reference type datatype , now array of string pointing towards indivisual string object in java . so if nothing is assigned to the array of string then it will be null.
+// String arr1[] = new String[10];
+// System.out.println(arr[0]);       =====>> it will give null  exception
