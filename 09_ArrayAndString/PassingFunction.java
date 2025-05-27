ca2ee3c9b9498e955a210d58d542d4badfa49518
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-05-27 18:37:12 +0530

    added more programs

--- 09_ArrayAndString/PassingFunction.java
+++ 09_ArrayAndString/PassingFunction.java
@@ -13,6 +13,21 @@ public class PassingFunction {
     }
 }
 
-// 1. Strings are immutable in java.
-// 2. Arrays are mutable in java
-// 3. Mutablity -> Their values can be modified at run time.
+// 1. Strings are immutable in Java:
+//    Once a String object is created, its value cannot be changed.
+//    Any operation that seems to modify a string (like concatenation or replacement)
+//    actually creates a new String object in memory. This ensures security and consistency,
+//    especially in multi-threaded environments.
+
+// 2. Arrays are mutable in Java:
+//    Arrays allow changes to their contents after creation. You can update, replace,
+//    or modify individual elements within the same array object. This makes arrays
+//    flexible for tasks like data manipulation or iterative processing.
+
+// 3. Mutability:
+//    An object is considered "mutable" if its internal state (its data or values)
+//    can be changed after it is created. If not, it is "immutable". Mutable objects
+//    are useful when you need to update or modify data over time,
+//    while immutable objects offer advantages like thread safety and simplicity.
+
+
