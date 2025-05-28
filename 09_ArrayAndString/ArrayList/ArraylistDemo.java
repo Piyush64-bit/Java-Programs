2e800bac4c6c07fc44fc53339af33b82b393f025
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-05-28 21:17:25 +0530

    added more programs

---
+++ 09_ArrayAndString/ArrayList/ArraylistDemo.java
@@ -0,0 +1,17 @@
+// Array List => DSA
+//   import java.util.Array List;
+//   import java.util.scanner;
+//   public class ArraylistDemo{
+
+//   }
+
+
+//   // 1.) when you don't know what will be the size  of an array & simply telling java  to handle the array and its element . there comes the arraylist in the game .
+//   // 2.) it is similar to vectors in c++ , in java it is the part of collections frameworks.
+  
+
+//   // Syntax.=>
+//    Arraylist<integer> list = new Arraylist <>();
+
+//    here integer means what is the type of  data you want to stop in the list .
+
