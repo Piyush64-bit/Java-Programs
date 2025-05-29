4a2f538d584730bdecc442525a2257bae8cda0b8
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-05-29 15:59:02 +0530

    added more programs

---
+++ 10_String/StringIntro.java
@@ -0,0 +1,24 @@
+
+public class StringIntro {
+
+    public static void main(String[] args) {
+        int str[] = {2, 3, 4, 5, 19}
+        int a = 10;
+        String name = "LAksh Yadav";
+        System.out.println(name);
+        String b = "ALksh";
+        b = "Yadav"
+        System.out.println(b);
+    }
+}
+
+// 1> Everything that starts with a capital letter ("s", "a") is a class in java.
+// 2> When you see the syntax of the string object, it is : Datatype refrence variable = string object
+// 3> Some important concepts of "String manipulation are-
+// a> String pool
+// b> Immutablity
+// a> Working os the string pool
+// String pool is the strorage data structure inside the heap memory, it is used to store the string odjects. Now, to optimize thius data structure prevents the entries of thye duplicate copies into the pool.
+// both are pointing towards a single object in the string pool(i.e. kunal), so no seperate as b object.
+// DOUTE
+// some of 
