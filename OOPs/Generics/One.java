b8f5b306e4a262df9135192674ec5df0a073189b
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-07-03 15:51:12 +0530

    added more program

---
+++ OOPs/Generics/One.java
@@ -0,0 +1,24 @@
+package OOPs.Generics;
+
+public class new {
+
+public void PrintList(List <?> mylist){
+ for(Object item : mylist){
+    System.out.println(item);
+ }
+}
+
+public double sumList(List <? extends Number> mylist){
+  double sum = 0.0 ;
+  for(Number num :mylist){
+    sum+= num.doubleValue();
+  }
+  return sum;
+}
+
+public class One {
+    public static void main(String[] args) {
+        
+    }
+}
+}
